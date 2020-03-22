966c1986a776753d8f7a12fb6de29f040e19dc62
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-03-21 22:46:04 -0300

    Declaraciones de variables

--- JavaSE/src/main/java/Variables.java
+++ JavaSE/src/main/java/Variables.java
@@ -1,13 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author linuxlite
- */
 public class Variables {
+
+    //Declaración de variables
+    int numeroInt = 10;
+    short numeroShort = 30;
+    double numeroDouble = 40.0;
+    long numeroLong = 50L;
+    float numeroFloat = 10F;
+    boolean numeroBoolean = true;
+    char caracter = 'h';
+    String cadena = "Hola Mundo";
+    
+    //Realice un metodo para mostrar en consola los valores de las variables
+    public void imprimir(){
+        System.out.println("Numero = " + this.numeroInt);
+        System.out.println("Numero = " + this.numeroShort);
+        System.out.println("Numero = " + this.numeroDouble);
+        System.out.println("Numero = " + this.numeroLong);
+        System.out.println("Numero = " + this.numeroFloat);
+        System.out.println("Caracter = " + this.caracter);
+        System.out.println("Cadena de texto = " + this.cadena);
+    }
+    
+
     
 }
