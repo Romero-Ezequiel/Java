7709191495bc5ced9192e4cf752a730ce5ff61fd
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-03-31 22:46:16 -0300

    Ejercicio con WHILE

---
+++ JavaSE/src/main/java/Ejercicio10.java
@@ -0,0 +1,14 @@
+/*
+10) Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+*/
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int numero =1;
+        
+        while( numero <= 100){
+            System.out.println(numero);
+            numero++;
+        }
+        
+    }
+}
