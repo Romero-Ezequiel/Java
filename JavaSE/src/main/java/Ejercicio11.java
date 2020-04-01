3fe7bd524dabfecf5578c1e1fa2ca7e1e3c0ead7
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-03-31 22:50:54 -0300

    Ejercicio con FOR

---
+++ JavaSE/src/main/java/Ejercicio11.java
@@ -0,0 +1,11 @@
+/*
+11) Haz el mismo ejercicio anterior con un bucle for.
+*/
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <=100; i++) {
+            System.out.println(i);
+        }
+    }
+}
