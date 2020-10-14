9d0b98d1de332e32f0a9fac76469def7118518a0
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-10-14 20:20:02 -0300

    Ejercicios de con arreglos en java

--- Arreglos/src/main/java/ar/com/arreglos/Ejemplo1.java
+++ Arreglos/src/main/java/ar/com/arreglos/Ejemplo1.java
@@ -1,12 +1,24 @@
 package ar.com.arreglos;
 
-
+//Creo un array que almacene 5 elementos y que me imprima 
+//un elemento del array
 public class Ejemplo1 {
-
 
     public static void main(String[] args){
         
+        
+        int [] miMatriz = new int[5];
+        
+        miMatriz[0] = 1;
+        miMatriz[1] = 2;
+        miMatriz[2] = 3;
+        miMatriz[3] = 4;
+        miMatriz[4] = 5;
+        
+        for(int i=0; i< miMatriz.length; i++){
+            System.out.println(miMatriz[i]);
+        }
+
+        
     }
-    
-    
 }
